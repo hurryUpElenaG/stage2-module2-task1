78fe37a676a6284f5abb0dcd0272cfa8e0a0fe31
ElenaGeorgievska <elena_science@yahoo.com>
2023-12-23 17:58:33 +0100

    task 1 servlets completed  http://localhost:8081/home

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,44 @@
 package com.example.servlet;
 
-public class AddUserServlet {
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+
     //write your code here!
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("jsp/add.jsp");
+        try {
+            requestDispatcher.forward(req, resp);
+        } catch (Exception i) {
+            i.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        User user = new User(firstName, lastName);
+        Warehouse war1 = Warehouse.getInstance();
+        war1.addUser(user);
+        req.setAttribute("user", user);
+        try {
+            resp.sendRedirect("/add");
+        } catch (Exception i) {
+            i.printStackTrace();
+        }
+
+    }
+
+
 }
